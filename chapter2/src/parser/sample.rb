159a4849fceb78efa494cfbc18efc45d08ab44ff
ishimura_shinichi <ishimura_shinichi@cyberagent.co.jp>
2014-10-23 11:42:30 +0900

    ちょっとparserを高機能に

--- chapter2/src/parser/sample.rb
+++ chapter2/src/parser/sample.rb
@@ -6,10 +6,29 @@ require 'treetop'
 p Treetop.load('simple')
 # => SimpleParser
 
-# パース
-parser_tree = SimpleParser.new.parse('while (x < 5) { x = x * 3 }')
+# パース関数
+def parse(syntax)
+  parser = SimpleParser.new
+  parser_tree = parser.parse(syntax)
+  unless parser_tree
+    p parser.failure_reason
+    p parser.failure_line
+    p parser.failure_column
+    raise 'Parse error.'
+  end
+  parser_tree
+end
+
+parser_tree = parse('while (x < 5) { x = x * 3 }')
 statement = parser_tree.to_ast
 p statement.class
 # => While
 p statement
 # => <<while(x < 5) { x = x * 3 }>>
+
+parser_tree = parse('if (false) { x = x * 3 } else { y = x + 3 }')
+statement = parser_tree.to_ast
+p statement.class
+# => If
+p statement
+# => <<if(false) { x = x * 3 } else { y = x + 3 }>>
